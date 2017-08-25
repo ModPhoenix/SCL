3976d1a4a30f11014449277c7ee8f68df25403e6
Unknown <modhands@gmail.com>
2017-08-25 19:39:03 +0300

    Profiles 3

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render, get_object_or_404
 
 from .models import User
+from blog.models import Post
 
 def user_profile(request, username):
     user_prolile = get_object_or_404(User, username=username)
-    context = {'user_prolile': user_prolile}
+    posts = Post.objects.filter(author=user_prolile)
+    context = {
+        'user_prolile': user_prolile,
+        'posts': posts,
+    }
     return render(request, 'profiles/profiles.html', context)
