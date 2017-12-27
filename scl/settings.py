62f5eb0734cc1f3074b77c5ff77154ffe7e028ff
MoDPhoenix <modhands@gmail.com>
2017-12-27 15:14:57 +0200

    Настройки

--- scl/settings.py
+++ scl/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g&v@a%9kqwv(lc*((w!qr$k&r6bwaa2*fidj89&=yzqo1(%c1_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['alpha.starcitizenlife.com', 'localhost', '127.0.0.1',]
 
