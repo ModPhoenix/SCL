aacc255357ae3c30a4d9e4f5687aabd4abae80bf
MoDPhoenix <modhands@gmail.com>
2018-01-01 22:14:44 +0200

    Настройка прав доступа для неавторизованых пользователей

--- guides/views.py
+++ guides/views.py
@@ -1,14 +1,18 @@
 from django.views.generic import ListView, DetailView, CreateView
 from django.views.generic.edit import UpdateView
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 from django.shortcuts import get_object_or_404
 
 from .models import Guide
+
 
 class GuideListView(ListView):
     context_object_name = 'guides'
     paginate_by = 10
     queryset = Guide.objects.all().select_related()
     template_name = 'guides/guides_index.html'
+
 
 class GuideDetailView(DetailView):
     model = Guide
@@ -19,6 +23,7 @@ class GuideDetailView(DetailView):
     template_name = 'guides/guide_detail.html'
 
 
+@method_decorator(login_required, name='dispatch')
 class GuideCreateView(CreateView):
     model = Guide
     fields = ['title', 'content', 'published']
@@ -28,13 +33,16 @@ class GuideCreateView(CreateView):
         form.instance.author = self.request.user
         return super(GuideCreateView, self).form_valid(form)
 
+
 class GuideUpdateView(UpdateView):
     model = Guide
     fields = ['title', 'content', 'published']
     template_name = 'blog/post_create.html'
 
+
 def create_guide(request):
     pass
+
 
 def edit_guide(request):
     pass
