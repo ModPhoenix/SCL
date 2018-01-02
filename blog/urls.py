9b7df1e6a8c9dd70ebf1bb4bfe4ffd529f0e7577
MoDPhoenix <modhands@gmail.com>
2018-01-02 16:59:12 +0200

    Фикс урл вконце всех /

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.PostList.as_view(), name='index'),
     url(r'^page/(?P<page>\d+)/$', views.PostList.as_view(), name='index_pagination'),
-    url(r'^(?P<id>\d+)-(?P<slug>[\w-]+)$', views.post_detail, name='detail'),
-    url(r'^create-post$', views.create_post, name='create_post'),
-    url(r'^(?P<id>\d+)-(?P<slug>[\w-]+)/edit$', views.edit, name='edit_post'),
+    url(r'^(?P<id>\d+)-(?P<slug>[\w-]+)/$', views.post_detail, name='detail'),
+    url(r'^create-post/$', views.create_post, name='create_post'),
+    url(r'^(?P<id>\d+)-(?P<slug>[\w-]+)/edit/$', views.edit, name='edit_post'),
 ]
--- guides/urls.py
+++ guides/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.GuideListView.as_view(), name='index'),
     url(r'^page/(?P<page>\d+)/$', views.GuideListView.as_view(), name='index_pagination'),
-    url(r'^(?P<id>\d+)-(?P<slug>[\w-]+)$', views.GuideDetailView.as_view(), name='guide_detail'),
-    url(r'^create-guide$', views.GuideCreateView.as_view(), name='create_guide'),
-    url(r'^(?P<id>\d+)-(?P<slug>[\w-]+)/edit$', views.GuideUpdateView.as_view(), name='edit_guide'),
+    url(r'^(?P<id>\d+)-(?P<slug>[\w-]+)/$', views.GuideDetailView.as_view(), name='guide_detail'),
+    url(r'^create-guide/$', views.GuideCreateView.as_view(), name='create_guide'),
+    url(r'^(?P<id>\d+)-(?P<slug>[\w-]+)/edit/$', views.GuideUpdateView.as_view(), name='edit_guide'),
 ]
