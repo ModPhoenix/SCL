de88ffe7901adf2721f3c87453e26a26d89c1e08
MoDPhoenix <modhands@gmail.com>
2018-01-05 23:30:21 +0200

    favicon.ico

--- scl/urls.py
+++ scl/urls.py
@@ -1,23 +1,10 @@
-"""scl URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.11/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.conf.urls.static import static
 from django.contrib.sitemaps.views import sitemap
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 
 from django.contrib.flatpages.views import flatpage
 
@@ -29,6 +16,7 @@ sitemaps = {
 }
 
 urlpatterns = [
+    url(r'^favicon.ico$',RedirectView.as_view(url=staticfiles_storage.url('img/favicon/favicon.ico')),name="favicon"),
     url(r'^', include('blog.urls', namespace='blog')),
     url(r'^guides/', include('guides.urls', namespace='guides')),
     url(r'^comments/', include('comments.urls', namespace='comments')),
@@ -51,6 +39,7 @@ urlpatterns = [
     url(r'^privacy/$', flatpage, {'url': '/privacy/'}, name='privacy'),
     url(r'^tellme/', include("tellme.urls")),
 ]
+
 
 if settings.DEBUG:
     import debug_toolbar
